ff6ae0a660caa02e186c1a7b2c9444173bb524bb
Naveen raj <117265644+naveen2131-hue@users.noreply.github.com>
2023-10-18 15:30:07 +0530

    updated password

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -3,7 +3,7 @@ public class Hello {
   
   public static String GREETING = "Hello world!";
   username: entry
-  password: 1@#$56*(%^
+  password: 1@#$56*%^!!
   
   public static void main(String []args) {
     
